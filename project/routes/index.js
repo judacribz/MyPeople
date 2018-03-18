9e3ca9bfc8ebc70cd8d4821e299f68d20caab28c
xXGirl-Computer-ScientistXx <mirical.williamscauston@uoit.net>
2018-03-18 16:24:16 -0400

    Renamed a few files and fixed a bug that caused my buttons on the login page to be shifted

--- project/routes/index.js
+++ project/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {title: 'My People'});
+  res.render('login', {title: 'Sign In | My People'});
 });
 
 module.exports = router;
