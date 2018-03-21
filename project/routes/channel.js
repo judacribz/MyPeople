841f3736ff3b7577bc6f9c31bd4e5cefdb99eb92
judacribz <sheron.balas@gmail.com>
2018-03-20 21:15:23 -0400

    added separate drop down menus to group and channel

--- project/routes/channel.js
+++ project/routes/channel.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/channel', function (req, res) {
-    res.render('channel', {title: 'CHANNEL_NAME'});
+    res.render('channel', {
+        title: 'CHANNEL_NAME | My People',
+        channelList: ["labs", "randChannel", "tests", "good"]
+    });
 });
 
 module.exports = router;
--- project/routes/group.js
+++ project/routes/group.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/group', function (req, res) {
-    res.render('group', {title: 'GROUP_NAME'});
+    res.render('group', {
+        title: 'GROUP_NAME | My People',
+        groupList: ["coolGroup", "randgroup", "tests"]
+    });
 });
+
+// router.get('/group/:groupId', function (req, res) {     res.render('group', {
+//         title: req.params + ' | My People ',         groupList: [
+// "coolGroup", "randgroup", "tests"         ],         groupId: req.params });
+// })
 
 module.exports = router;
