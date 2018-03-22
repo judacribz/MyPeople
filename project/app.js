1f376f468754135fdd455c744011e465b05c6059
Dikachi <dikachik@yahoo.com>
2018-03-22 19:18:36 -0400

    added the mongoose to the javascript

--- project/app.js
+++ project/app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var uuid = require('uuid/v1');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var assert = require('assert');
+
 var path = require('path');
 var nodemon = require('nodemon');
 
@@ -23,5 +29,43 @@ app.use('/', require('./routes/channel'));
 app.listen(app.get('port'), function () {
   console.log('Node.js/Express is listening on http://localhost:' + app.get('port') + '/');
 });
+
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://localhost:27017/myPeople');
+
+  app.use(session({
+  genid: function(request) {
+    return uuid();
+  },
+  resave: false,
+  saveUninitialized: false,
+  //cookie: {secure: true},
+  secret: 'apollo slackware prepositional expectations'
+}));
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  username: {type: String,
+             unique: true,
+             index: true},
+  email: String,
+  usercontacts: [String]
+}, {collection: 'users'});
+var User = mongoose.model('user', userSchema);
+
+var GroupSchema = new Schema({
+    channels: [{
+        members: String,
+        messages: [{
+            username: String,
+            content: String,
+            timestamp: String
+          }
+        ]
+      }
+    ]
+}, {collection: 'groups'});
+var Group = mongoose.model('group', GroupSchema);
+
 
 module.exports = app;
