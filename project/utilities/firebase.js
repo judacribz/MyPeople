3e4c4092ed5003ebcb4bebfe1b398325230c2a6a
judacribz <sheron.balas@gmail.com>
2018-03-29 14:24:02 -0400

    basic messaging added, messages displayed in console when user enters msg. This will be displayed for all users. //TODO: make only users part of channel be able to see new updates once adding username is implemented

--- project/utilities/firebase.js
+++ project/utilities/firebase.js
@@ -4,10 +4,18 @@ const fbConfig = require('../config/firebase.config')
 module.exports = {
     setupFirebase: function () {
         firebase.initializeApp(fbConfig);
+    },
 
-        var database = firebase.database();
-        var starCountRef = firebase.database().ref('groups/');
-        starCountRef.on('child_added', function (snapshot) {
+    fbPushMessage: function(username, message) {
+        var messages = firebase.database().ref('groups/HackerGroup-2018/events/messages/');
+        var updates = {};
+        updates[Date.now()] = {username: "Joe", content: message};
+         messages.update(updates);
+    },
+
+    getChannelUpdates: function() {
+        var messages = firebase.database().ref('groups/HackerGroup-2018/events/messages/');
+        messages.on('child_added', function (snapshot) {
             console.log(snapshot.val());
         });
     }
