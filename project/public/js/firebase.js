3e216ff7c1e4b16dc6d18ce98f614d47841bdb1b
judacribz <sheron.balas@gmail.com>
2018-03-29 14:27:43 -0400

    update gitignore with logs dir

--- project/public/js/firebase.js
+++
@@ -1,7 +0,0 @@
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-var firebase = require("firebase");
-
-$(document).ready(function() {
-    var db = firebase.database();
-});
