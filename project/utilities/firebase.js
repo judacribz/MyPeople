51b83723c29fd725eb5c6d9e7f25da049fa9c4d5
judacribz <sheron.balas@gmail.com>
2018-04-01 12:37:09 -0400

    fixed pug file errors

--- project/utilities/firebase.js
+++ project/utilities/firebase.js
@@ -1,22 +1,27 @@
 const firebase = require("firebase");
-const fbConfig = require('../config/firebase.config')
+const fbConfig = require('../config/firebase.config');
+const cheerio = require('cheerio');
+const $ = cheerio.load('<div id="chatArea">...</div>');
 
 module.exports = {
     setupFirebase: function () {
         firebase.initializeApp(fbConfig);
     },
 
-    fbPushMessage: function(username, message) {
+    fbPushMessage: function (username, message) {
         var messages = firebase.database().ref('groups/HackerGroup-2018/events/messages/');
         var updates = {};
-        updates[Date.now()] = {username: "Joe", content: message};
-         messages.update(updates);
+        updates[Date.now()] = {
+            username: "Joe",
+            content: message
+        };
+        messages.update(updates);
     },
 
-    getChannelUpdates: function() {
+    getChannelUpdates: function () {
         var messages = firebase.database().ref('groups/HackerGroup-2018/events/messages/');
         messages.on('child_added', function (snapshot) {
-            console.log(snapshot.val());
+            $('#chatArea').text(message);
         });
     }
 };
