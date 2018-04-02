7b10f808346277748c6c0aed78c7a47cd2bcafc8
judacribz <sheron.balas@gmail.com>
2018-04-01 21:33:40 -0400

    Added a Procfile.

--- project/utilities/firebase.js
+++ project/utilities/firebase.js
@@ -11,7 +11,6 @@ module.exports = {
     fbPushMessage: function (username, message) {
         var messages = firebase.database().ref('groups/HackerGroup-2018/events/messages/');
         var updates = {};
-
         updates[Date.now()] = {
             username: "Joe",
             content: message
