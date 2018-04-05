902ba0560069357f5ed36fdef47bd7e7cb68b8b4
judacribz <sheron.balas@gmail.com>
2018-04-05 11:11:13 -0400

    fixing linux issues

--- project/build.js
+++ project/build.js
@@ -2,26 +2,28 @@ const sys = require('util');
 const exec = require('child_process').exec;
 const os = require('os');
 
+const mongoConfig = "./config/mongod_win.conf";
+const startMongo = "mongod";
+const startMongoWin = "start " + startMongo;
+
 function buildEnv() {
     if (os.type() === 'Linux') {
-        logEnv('Linux');
-        exec("mongod -f ./config/mongod_lnx.conf", puts);
+        logEnv('Linux', startMongo);
     } else if (os.type() === 'Darwin') {
-        logEnv('Mac');
-        exec("mongod -f ./config/mongod_lnx.conf", puts);
+        logEnv('Mac', startMongo);
     } else if (os.type() === 'Windows_NT') {
-        logEnv('Windows');
-        exec("start mongod -f ./config/mongod_win.conf", puts);
+        logEnv('Windows', startMongoWin);
     } else
         throw new Error("Unsupported OS found: " + os.type());
 }
 
-function puts(error, stdout, stderr) {
-    console.log(stdout);
+function logEnv(osType, cmd) {
+    console.log(osType + " environment detected.");
+    exec(cmd + " -f " + mongoConfig, puts);
 }
 
-function logEnv(osType) {
-    console.log(osType + " environment detected.");
+function puts(error, stdout, stderr) {
+    console.log(stdout);
 }
 
 module.exports = buildEnv;
