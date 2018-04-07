3bc1e333a331ecde309bec3212c6dc940e7a32ab
Dikachi <dikachik@yahoo.com>
2018-04-07 13:28:26 -0400

    gets usernames now

--- project/routes/message.js
+++ project/routes/message.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const fb = require("../utilities/firebase");
 var router = express.Router();
+
+var rootRef = fb.firebase.database().ref().child("users");
+
+  var username = [];
+  rootRef.on("child_added", snap =>{
+   var user = snap.child("username").val();
+   username.push(user);
+  });
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('message', {
         title: 'DIRECT MESSAGE | My People',
-        channelList: ["Dikachi", "Mirical", "Sharan", "Random"]
+        channelList: username
     });
 });
 
