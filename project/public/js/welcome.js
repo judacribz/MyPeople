ad947b000b3c45ac2053c3f25f75eef9dda50ee3
xXGirl-Computer-ScientistXx <mirical.williamscauston@uoit.net>
2018-04-08 23:41:40 -0400

    Welcome page is finished, when user signs they will be presented with channels to go to, if user clicks on a channel it will take them to the channel.pug page (Because we don't have multiple channel pages created just yet, I only linked it to the channel.pug page for now)

--- project/public/js/welcome.js
+++ project/public/js/welcome.js
@@ -1,15 +1,21 @@
-// var userId = firebase.auth().currentUser.uid;
-// const dbRefObject = firebase.database().ref().child('/users/' + userId + '/');
-
 $(document).ready(function () {
-	// firebase.auth().onAuthStateChanged(user => {
+	const fbPath = 'https://mypeople-5d5e0.firebaseio.com';
+	var fbDatabase = firebase.database();
 
-	// });
+	/* Populate the list group with user channels */
+	var groups = fbDatabase.ref('groups/');
+	groups.on('child_added', function(groupShot) {
+		$('.list-group').append("<a class='list-group-item list-group-item-action'>"+groupShot.key+ "</a>");
+	});
 
-	// dbRefObject.on('value', function(userShot) {
-	// 	var username = userShot.val().username;
-	// });
+	/* If list group item is clicked then take the user to the respective channel
+	TODO: Get list group item clicked and take user to the respective channel page, 
+	so far if user clicks on any list group item, it will take user to channel.pug page */
+	$('.list-group').click(function() {
+		window.location = '/channel';
+	});
 
+	/* Log the user out when logout is clicked */
 	$('#logout').click(function () {
 		firebase.auth().signOut();
 		window.location = '/';
